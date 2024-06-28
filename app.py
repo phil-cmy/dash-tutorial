8c179fb3b64835bb94461ad34c414c7b702098b9
phil-cmy <philchen1995@gmail.com>
2024-06-27 23:43:07 -0700

    interactive components

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Input, Output
 import pandas as pd
+import plotly.express as px
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
+# Initialize the app
 app = Dash()
 
+# App layout
 app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+    html.Div(children='My First App with Data, Graph, and Controls'),
+    html.Hr(),
+    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-items'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+    dcc.Graph(figure={}, id='controls-and-graph')
 ]
 
+# Add controls to build the interaction
 @callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-items', component_property='value')
 )
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+def update_graph(selected_value):
+    fig = px.histogram(df, x='continent', y=selected_value, histfunc='avg', title=f'Average {selected_value} by Continent')
+    return fig
 
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
